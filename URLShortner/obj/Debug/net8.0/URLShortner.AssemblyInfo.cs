b7c576fc35cb873405c8a54aa77e72b111e1e6c8
Gustavo Griffo <gustavo.griffo@gmail.com>
2024-02-23 16:57:14 -0600

    Project Publishing

--- URLShortner/obj/Debug/net8.0/URLShortner.AssemblyInfo.cs
+++ URLShortner/obj/Debug/net8.0/URLShortner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("URLShortner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0abebe80a8a75dcf45c0572a485d9972df3f4be")]
 [assembly: System.Reflection.AssemblyProductAttribute("URLShortner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("URLShortner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
